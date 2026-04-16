ebf184e16f8ae7bc244e3f96d772063eb5613369
Nguyễn Hữu Duy <huuduy.uwu@gmail.com>
2026-04-16 16:00:57 +0700

    Initial commit

---
+++ src/test/java/com/desertrose/DesertroseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desertrose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesertroseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
